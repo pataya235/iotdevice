9387a7239b8b1658f8cf5f5cf1d486e2333c575e
assmanfe <felix.assmann@dormakaba.com>
2024-12-16 15:17:01 +0100

    fix led handling

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,6 @@ void setup() {
   digitalWrite(LED, LOW);
 
   Serial.println();
-  Serial.println();
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -37,31 +36,26 @@ void loop() {
     return;
   }
 
-  Serial.println("new client");
-  while (! client.available())
-  {
-    delay (1);
-  }
-
+  Serial.println("Client connected");
   String req = client.readStringUntil('\r');
   Serial.println(req);
   client.flush();
 
   if (req.indexOf("/ledoff") != -1) {
-    status_led=0;
-    digitalWrite(LED, LOW);
+    status_led=1;
+    digitalWrite(LED, HIGH);
     Serial.println("LED OFF");
   }
   else if(req.indexOf("/ledon") != -1)
   {
-    status_led=1;
-    digitalWrite(LED,HIGH);
+    status_led=0;
+    digitalWrite(LED,LOW);
     Serial.println("LED ON");
   }
 
   client.println("<!DOCTYPE HTML>");
-  client.println("<HTML>");
-  client.println("<head><meta name=\"viewport\" content=\"width=device-width, inital-scale=1\">");
+  client.println("<html>");
+  client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
   client.println("<link rel=\"icon\" href=\"data:,\">");
   // CSS to style the on/off buttons
   client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
@@ -69,13 +63,14 @@ void loop() {
   client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
   client.println(".button2 {background-color: #008000; border: none; color: white; padding: 16px 40px;");
   client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+  client.println("</style></head>");
   // Web Page Heading
-  client.println("<h1>LED CONTROL<h1>");
+  client.println("<body><h1>LED CONTROL</h1>");
   // Display and ON/OFF buttons
   client.println("<p><a href=\"/ledon\"><button class=\"button\">ON</button></a></p>");
-  client.println("<p><a href=\"/ledoff\"><button class=\"button2\">");
+  client.println("<p><a href=\"/ledoff\"><button class=\"button2\">OFF</button></a></p>");
   client.println("</body></html>");
-  delay(1);
-  Serial.println("Client disonnected");
-  Serial.println("");
+
+  client.stop();
+  Serial.println("Client Disconnected");
 }
